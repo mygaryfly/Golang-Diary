b9c25ed3d3ed13c18151c6d9cc4442cfb8aa3d8d
Gu Qianliang[ANJI-CEVA IEBU] <guqianliang@anji-ceva.local>
2019-11-25 16:12:34 +0800

    d7gotest01

--- Codes/test.go
+++ Codes/test.go
@@ -29,3 +29,26 @@ func m() {
 	print(a)
 }
 >>>>>>> 12b184ea8c350dba2a34b68fa820e19542c93c81
+package main
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+func main() {
+	for i := 0; i < 10; i++ {
+		a := rand.Int()
+		fmt.Printf("%d / ", a)
+	}
+	for i := 0; i < 5; i++ {
+		r := rand.Intn(8)
+		fmt.Printf("%d / ", r)
+	}
+	fmt.Println()
+	timens := int64(time.Now().Nanosecond())
+	rand.Seed(timens)
+	for i := 0; i < 10; i++ {
+		fmt.Printf("%2.2f / ", 100*rand.Float32())
+	}
+}
