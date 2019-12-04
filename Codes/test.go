a9f94701920c3a2fb8d2f4df8b98b299c626075e
mygaryfly <mygaryfly@163.com>
2019-12-04 21:48:39 +0800

    d11bmd3

--- Codes/test.go
+++ Codes/test.go
@@ -1,56 +1,15 @@
 package main
 
-<<<<<<< HEAD
 import (
 	"fmt"
-   "runtime"
-	"os"
 )
 
 func main() {
-	var goos string = runtime.GOOS
-	fmt.Printf("The operating system is: %s\n", goos)
-	path := os.Getenv("PATH")
-	fmt.Printf("Path is %s\n", path)
-}
-=======
-var a = "G"
+	power := 1000
+	fmt.Printf("default power is %d\n", power)
 
-func main() {
-	n()
-	m()
-	n()
-}
-
-func n() { print(a) }
-
-func m() {
-	a := "O"
-	print(a)
-}
->>>>>>> 12b184ea8c350dba2a34b68fa820e19542c93c81
-package main
-import (
-	"fmt"
-	"math/rand"
-	"time"
-)
-
-func main() {
-	for i := 0; i < 10; i++ {
-		a := rand.Int()
-		fmt.Printf("%d / ", a)
-	}
-	for i := 0; i < 5; i++ {
-		r := rand.Intn(8)
-		fmt.Printf("%d / ", r)
-	}
-	fmt.Println()
-	timens := int64(time.Now().Nanosecond())
-	rand.Seed(timens)
-	for i := 0; i < 10; i++ {
-		fmt.Printf("%2.2f / ", 100*rand.Float32())
-	}
+	name, power := "Goku", 9000
+	fmt.Printf("%s's power is over %d\n", name, power)
 }
 
 //https://github.com/unknwon/go-fundamental-programming/blob/master/lectures/lecture1.md
