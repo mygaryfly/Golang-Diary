cd6a6c300745375cee831d1b74a24e9d8e2da4b5
mygaryfly <mygaryfly@163.com>
2019-12-09 23:32:27 +0800

    Merge branch 'master' of E:\L77workpath\Projects\Golang-Diary with conflicts.

--- Codes/test.go
+++ Codes/test.go
@@ -2,29 +2,6 @@ package main
 
 import (
 	"fmt"
-<<<<<<< HEAD
-	"strconv"
-)
-
-func main() {
-	var orig string = "ABC"
-	// var an int
-	var newS string
-	// var err error
-
-	fmt.Printf("The size of ints is: %d\n", strconv.IntSize)	  
-	// anInt, err = strconv.Atoi(origStr)
-	an, err := strconv.Atoi(orig)
-	if err != nil {
-		fmt.Printf("orig %s is not an integer - exiting with error\n", orig)
-		return
-	} 
-	fmt.Printf("The integer is %d\n", an)
-	an = an + 5
-	newS = strconv.Itoa(an)
-	fmt.Printf("The new string is: %s\n", newS)
-}
-=======
 )
 
 func main() {
@@ -36,4 +13,3 @@ func main() {
 }
 
 //https://github.com/unknwon/go-fundamental-programming/blob/master/lectures/lecture1.md
->>>>>>> f7594bbeabc11cbfb341467a2c5ec2a6b0467b62
